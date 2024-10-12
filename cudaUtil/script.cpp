ebd071b6477b6e4823d507698d1a122298467626
Admineus <freapq@gmail.com>
2024-10-12 14:46:28 +0300

    Create script.cpp

---
+++ cudaUtil/script.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <iomanip>
+
+using namespace std;
+using namespace boost::multiprecision;
+
+int main() {
+    // Определяем максимальное значение
+    cpp_int max_value("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140");
+    cpp_int start_value("0x1");
+
+    // Итерируем от start_value до max_value
+    for (cpp_int i = start_value; i <= max_value; ++i) {
+        // Выводим текущее значение в шестнадцатичном формате
+        cout << hex << setfill('0') << setw(64) << i << endl;
+    }
+
+    return 0;
+}
